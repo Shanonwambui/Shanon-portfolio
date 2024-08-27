ed3867102ba55fe39e5528bea66d19241aea5c85
shanon <shanonwambui24@gmail.com>
2024-08-28 02:14:45 +0300

    changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,7 +23,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 0.7 : 0.7}
-        position={isMobile ? [0, -3.1, -2.2] : [0, -3.7, -1.5]}
+        position={isMobile ? [0, -2.9, -2.2] : [0, -3.7, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
