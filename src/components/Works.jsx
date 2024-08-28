1d57f51adbdd998998d8bc26b1ac43313020c824
shanon <shanonwambui24@gmail.com>
2024-08-28 15:17:05 +0300

    changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -49,10 +49,15 @@ const ProjectCard = ({
           </div>
         </div>
 
-        <div className='mt-5' onClick={() => window.open(demo, "_blank")}>
-          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
-        </div>
+        <a 
+  href={demo} 
+  target="_blank" 
+  rel="noopener noreferrer" 
+  className='block mt-5'>
+    <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+    <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+</a>
+
 
         <div className='mt-4 flex flex-wrap gap-2'>
           {tags.map((tag) => (
